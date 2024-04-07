b3b4770575ff9dc6a64fee94a97deb4f704e9cb8
Kurt <wedku875@student.otago.ac.nz>
2024-04-07 14:58:30 +1200

    comment fixing

--- 678 Valid Parenthesis String/ValidParenthesisString.java
+++ 678 Valid Parenthesis String/ValidParenthesisString.java
@@ -13,11 +13,10 @@ public class ValidParenthesisString {
         // doing the same thing backwards for edge cases
         int tempA = 0,  tempB = 0, tempStar = 0;
         for(int i = s.length()-1; i >= 0; i--){
-            if(s.charAt(i) == ')') tempA++; // if there is a ) add to the count
-            else if(s.charAt(i) == '(') tempB++; // if there is a ( take 1 from the count
-            // if the count is 0, there is an equal number of ('s and )'s
-            else if(s.charAt(i) == '*') tempStar++; // this can be counted as a ( or )
-            if(tempA + tempStar < tempB) return false; // if at any time the possible number of open brackets is less than the closing ones, return false
+            if(s.charAt(i) == ')') tempA++;
+            else if(s.charAt(i) == '(') tempB++;
+            else if(s.charAt(i) == '*') tempStar++;
+            if(tempA + tempStar < tempB) return false; // if at any time the possible number of closed brackets is less than the opening ones, return false
         }
 
         System.out.println(leftParenthesesCount + ", " + rightParenthesesCount + ", " + starCount);

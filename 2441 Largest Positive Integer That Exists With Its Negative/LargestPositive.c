ad2c61eb26efa50c212a99d20db246bb644ff746
kurtwedding <kurtwedding@gmail.com>
2024-05-02 20:41:14 +1200

    Made the C solution a little bit more efficient

--- 2441 Largest Positive Integer That Exists With Its Negative/LargestPositive.c
+++ 2441 Largest Positive Integer That Exists With Its Negative/LargestPositive.c
@@ -5,13 +5,14 @@ int findMaxK(int* nums, int numsSize) {
     int max = -1;
     for (int i = 0; i < numsSize; i++){ // First iteration
         bool hasNegative = false;
-        for (int j = 0; j < numsSize; j++){ // Second iteration
-            if(nums[j] == -nums[i]) {
-                hasNegative = true;
-                break;
-            };
+        if(nums[i] > max){ // only check if the negative exists in the array if the value is larger than the current max
+            for (int j = 0; j < numsSize; j++){ // Second iteration
+                if(nums[j] == -nums[i]) { 
+                    max = nums[i];
+                    break; // Can break out once the negative of it is found
+                };
+            }
         }
-        if(hasNegative && nums[i] > max) max = nums[i];
     }
     return max;
 }

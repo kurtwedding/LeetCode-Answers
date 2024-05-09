a26c640abe840a1984df550d87e14c7b2ff901c7
kurtwedding <kurtwedding@gmail.com>
2024-05-09 21:31:46 +1200

    This solution is a lot cleaner, and works a lot better.

--- 3075 Maximize Happiness of Selected Children/MaximumHappiness.js
+++ 3075 Maximize Happiness of Selected Children/MaximumHappiness.js
@@ -4,34 +4,19 @@
  * @return {number}
  */
 var maximumHappinessSum = function(happiness, k) {
+    happiness.sort((i, j) => j - i) // sort in descending order
     var sum = 0;
-    for(var i = 0; i < k; i++){
-        console.log(happiness)
-        var index = findMaxIndex(happiness)
-        sum += happiness[index];
-        happiness.splice(index, 1);
-        for(var j = 0; j < happiness.length; j++){
-            if(happiness[j] > 0) happiness[j]--;
-        }
+
+    // now sum all the values in the array from left to right, decreasing each element by 1 each time 
+    for(var i = 0; i < k; i++) {
+        if(happiness[i] < 1) break; // if all the remaining elements are 0 or lower, break
+        sum += Math.max(0, happiness[i] - i); // making sure the value can never go negative
     }
 
-    return sum
+    return sum;
 };
 
-function findMaxIndex(array){
-    var max = 0;
-    var index = 0;
-    for(var ele in array){
-        if (array[ele] > max) {
-            max = array[ele]
-            index = ele
-        };
-    }
-
-    return index;
-}
-
-const input = [2,98,45];
-const k = 1
+const input = [12,1,42];
+const k = 3
 
 console.log(maximumHappinessSum(input, k))
